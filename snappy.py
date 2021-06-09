e0526143cb0ffae3472758818f5e03ebaf815537
Eduardo Rodriguez <erodri07@gmail.com>
2021-06-08 21:37:46 -0400

    otro comiit

--- snappy.py
+++ snappy.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 # Junio 2021
 # Project Snappy
 
 print("Hello World")
-=======
-# Junio 2021
-# Project Snappy
-
-print("Hello World")
->>>>>>> 8013143ddbfe8949076885bf8b10f882168ca70c
